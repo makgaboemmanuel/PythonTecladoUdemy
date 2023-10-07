8e0890e3e7a26fee6a8c62648def04979103fefd
gudlukmme@gmail.com <gudlukmme@gmail.com>
2023-10-07 17:07:58 +0200

    Strings In Python

--- main.py
+++ main.py
@@ -12,5 +12,55 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    age = 35
+    name = 'Makgabo Emmanuel Mathekga'
+    print('Dealing With Variables')
+    print(name, age)
 
+    # variables in capital letters are considered constants and should not be changed
+    PI = 3.14
+    PI = PI * 2
+    print('PI value is:', PI)
+
+    RADIANS_DEGREES = 180 / PI
+
+    # important note, when you do a division in python, the returned data type is a float. even if there is no remainder
+    # to avoid the comma after the division, use // , the second division sign removes the comma
+    # this only works if there is no remainder
+
+    print('Float value after division: ', RADIANS_DEGREES)
+    print('Float value after division: ', 10 / 5)
+    print('Integer value after division: ', 10 // 5)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    # remainder in Python - uses the modulus % operator
+    modulus_example = 6 % 5
+    print('Remainder example: ', modulus_example)
+    # using the modulus operator to determine the odd and even numbers
+    odd_even = 2 % 2
+    if odd_even != 0:
+        print('Not an even number')
+    else:
+        print('It is an even number')
+
+    # use double quotes if you have single quotes inside the string.
+    # otherwise escape the character to ensure the quote is part of the value if the string
+    string_ex = "Makgabo\' Emmanuel Mathekga"
+
+    print('String example: ', string_ex)
+
+    # multi-line strings: you must use triple string quotes at beginning and end
+    newspaper = """Bomma ke boss
+    O fetsa puff
+    Idibala
+    Ke gopotse ex yaka 
+    """
+    print(newspaper)
+
+    # you can also use multi line strings as comments without the variable name
+    """"
+        this is a comment
+        and won't be used anywhere in the code
+        if you have long text that you want to write
+    """
+
