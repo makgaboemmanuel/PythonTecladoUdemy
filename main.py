a2eaecb2c794040193ed2b6a44a3fcbf4f91f9ba
Mathekga Makgabo Emmanuel <gudlukmme@gmail.com>
2023-10-09 11:41:29 +0200

    Update main.py
    09-October-2023 and as well as or statements

--- main.py
+++ main.py
@@ -56,6 +56,9 @@ if __name__ == '__main__':
     Ke gopotse ex yaka 
     """
     print(newspaper)
+    final_greeting = "Good day {person}"
+    my_greeting = final_greeting.format(person=string_ex)
+    print(my_greeting)
 
     # you can also use multi line strings as comments without the variable name
     """"
@@ -63,4 +66,26 @@ if __name__ == '__main__':
         and won't be used anywhere in the code
         if you have long text that you want to write
     """
+    # dealing with user input
 
+    print("Please provide your name")
+    my_name = input()
+    print(f"Your name is {my_name}")
+    # important:
+    # int * int = int
+    # float * int = float
+    # string * int = string will be printed the number of int times
+    print(f"You are {6*'d'} months old")
+    # converting: input always returns a string data type, you have to explictly convert it to any other format
+    print("Please provide your salary")
+    salary = int(input())
+    print(f"Your salary is {salary}") # .format(salary)
+
+    # comparison statements in Python:
+    print(f"Using comparison statements: 5 > 3: ", ( 5 > 3))
+    # == compares the equality of two values
+    print(f"Using comparison statements: 5 == 3: ", ( 5 == 3))
+
+    # dealing with and as well as or
+    results = salary < 65 and len(my_name) > 10
+    print("Using and as wel as or:", results)
